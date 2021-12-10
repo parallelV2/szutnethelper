0e909cd69c78cf02a66168e9985d9ac278cf3e46
2huo <maipianlaoxu@gmail.com>
2021-12-10 11:00:17 +0800

    init

--- electron/main.ts
+++ electron/main.ts
@@ -1,10 +1,23 @@
+import { join } from 'path';
 import { app } from 'electron';
 import Runtime, { winService } from '@modern-js/runtime/electron-main';
+
+const PRELOAD_JS = join(
+  __dirname,
+  'preload',
+  process.env.NODE_ENV === 'development' ? 'index.dev.js' : 'index.js',
+);
 
 const runtime = new Runtime({
   windowsConfig: [
     {
       name: 'main',
+      options: {
+        webPreferences: {
+          preload: PRELOAD_JS,
+          enableRemoteModule: true,
+        },
+      },
     },
   ],
   mainServices: {
